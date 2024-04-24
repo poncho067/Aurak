d035eec851bed79d545a210f60daf63450bfd32a
Poncho <alfonsocg06@gmail.com>
2024-04-23 20:30:45 -0600

    3pasos

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
 
-menu.addEventListener('click', function() {
-  menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('active');
-})
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
 
-document.querySelectorAll('.navbar__links').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      e.preventDefault();
+document.querySelectorAll(".navbar__links").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-      const targetId = this.getAttribute('href').substring(1);
-      const target = document.getElementById(targetId);
+    const targetId = this.getAttribute("href").substring(1);
+    const target = document.getElementById(targetId);
 
-      const navbarHeight = document.querySelector('.navbar').offsetHeight;
-      const offsetTop = target.getBoundingClientRect().top + window.pageYOffset - navbarHeight;
+    const navbarHeight = document.querySelector(".navbar").offsetHeight;
+    const offsetTop =
+      target.getBoundingClientRect().top + window.pageYOffset - navbarHeight;
 
-      window.scrollTo({
-          top: offsetTop,
-          behavior: 'smooth'
-      });
+    window.scrollTo({
+      top: offsetTop,
+      behavior: "smooth",
+    });
   });
 });
 
-document.querySelectorAll('.navbar__links').forEach(link => {
-  link.addEventListener('click', function() {
-      const mobileMenu = document.getElementById('mobile-menu');
-      if (mobileMenu.classList.contains('is-active')) {
-          mobileMenu.classList.remove('is-active');
-          // Hide the navbar menu
-          const navbarMenu = document.querySelector('.navbar__menu');
-          navbarMenu.classList.remove('active');
-      }
+document.querySelectorAll(".navbar__links").forEach((link) => {
+  link.addEventListener("click", function () {
+    const mobileMenu = document.getElementById("mobile-menu");
+    if (mobileMenu.classList.contains("is-active")) {
+      mobileMenu.classList.remove("is-active");
+      // Hide the navbar menu
+      const navbarMenu = document.querySelector(".navbar__menu");
+      navbarMenu.classList.remove("active");
+    }
   });
 });
